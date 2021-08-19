511ebbdc9829d033c95fe02cbefdfe68d46b5c9c
Lahiru Samishka <lahirusamishka@gmail.com>
2021-08-19 17:30:33 +0530

    request application

---
+++ src/src/app/core/services/loan-service.service.ts
@@ -0,0 +1,43 @@
+import { tap } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { RuntimeEnvLoaderService } from "./runtime-env-loader.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class LoanServiceService {
+
+  constructor(
+    private http: HttpClient,
+    private envLoader: RuntimeEnvLoaderService
+  ) {}
+
+  borrowerSave(data): Observable<any> {
+    return this.http.post(this.apiPath("/borrower"), data).pipe(
+      tap((data) => {
+        return data;
+      })
+    );
+  }
+
+  checkUserOldApplication(userId): Observable<any> {
+    return this.http.get(this.apiPath("/borrower/" + userId)).pipe(
+      tap((data) => {
+        return data;
+      })
+    );
+  }
+
+  getLogUserId() {
+    const json = localStorage.getItem("currentUser");
+    return JSON.parse(json).id;
+  }
+
+  private apiPath(path): string {
+    return (
+      this.envLoader.env.API_BASE_URL + "/" + this.envLoader.env.APP_KEY + path
+    );
+  }
+}
--- src/src/app/request/request/request.component.ts
+++ src/src/app/request/request/request.component.ts
@@ -4,41 +4,94 @@ import { NotificationService } from "./../../core/services/notification.service"
 import { Title } from "@angular/platform-browser";
 import { Router } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
-var domToPdf = require('dom-to-pdf');
+import domToPdf from "dom-to-pdf";
+import * as moment from "moment";
+import { DatePipe } from "@angular/common";
+import { LoanServiceService } from "src/app/core/services/loan-service.service";
 @Component({
   selector: "app-request",
   templateUrl: "./request.component.html",
   styleUrls: ["./request.component.css"],
 })
 export class RequestComponent implements OnInit {
-  loginForm: FormGroup;
+  requestForm: FormGroup;
   loading: boolean;
+  userId: string;
+
   constructor(
     private router: Router,
     private titleService: Title,
     private notificationService: NotificationService,
-    private authenticationService: AuthenticationService
+    private loanService: LoanServiceService
   ) {}
 
   ngOnInit() {
     this.titleService.setTitle("easyloan - request");
     this.createForm();
+    this.userId = this.loanService.getLogUserId();
+    this.checkIsHaveApplication();
+  }
+
+  checkIsHaveApplication() {
+    this.loanService.checkUserOldApplication(this.userId).subscribe((res) => {
+      if (res != null) {
+        console.log(res);
+        this.fillTheBorrowerData(res);
+      }
+    });
+  }
+
+  fillTheBorrowerData(data) {
+    this.requestForm.get("address1").setValue(data.address1),
+      this.requestForm.get("address2").setValue(data.address2),
+      this.requestForm.get("city").setValue(data.city),
+      this.requestForm.get("description").setValue(data.description),
+      this.requestForm.get("disbursed").setValue(data.disbursed),
+      this.requestForm.get("dob").setValue(new Date(data.dob)),
+      this.requestForm.get("email").setValue(data.email),
+      this.requestForm.get("first_name").setValue(data.first_name),
+      this.requestForm.get("interest").setValue(data.interest),
+      this.requestForm.get("last_name").setValue(data.last_name),
+      this.requestForm.get("loan_product").setValue(data.loan_product),
+      this.requestForm.get("middle_name").setValue(data.middle_name),
+      this.requestForm.get("mobile").setValue(data.mobile),
+      this.requestForm.get("postal_code").setValue(data.postal_code),
+      this.requestForm.get("state").setValue(data.state);
+      this.requestForm.get("working_status").setValue(data.working_status);
+      this.requestForm.get("title").setValue(data.title);
   }
 
   private createForm() {
-    const savedUserEmail = localStorage.getItem("savedUserEmail");
-
-    this.loginForm = new FormGroup({
-      email: new FormControl(savedUserEmail, [
+    this.requestForm = new FormGroup({
+      email: new FormControl("", [
         Validators.required,
         Validators.email,
       ]),
-      password: new FormControl("", Validators.required),
+      first_name: new FormControl("", [Validators.required]),
+      city: new FormControl("", [Validators.required]),
+      state: new FormControl("", [Validators.required]),
+      postal_code: new FormControl("", [Validators.required]),
+      address1: new FormControl("", [Validators.required]),
+      address2: new FormControl("", []),
+      last_name: new FormControl("", []),
+      description: new FormControl("", []),
+      middle_name: new FormControl("", [Validators.required]),
+      mobile: new FormControl("", [Validators.required]),
+      dob: new FormControl("", [Validators.required]),
+      title: new FormControl("", [Validators.required]),
+      working_status: new FormControl("", [Validators.required]),
       loan_product: new FormControl(
-        { value: "personal_loans", disabled: false },
+        { value: "", disabled: false },
         Validators.required
       ),
-      rememberMe: new FormControl(savedUserEmail !== null),
+      disbursed: new FormControl(
+        { value: "", disabled: false },
+        Validators.required
+      ),
+      interest: new FormControl(
+        { value: "", disabled: false },
+        Validators.required
+      ),
     });
   }
 
@@ -53,25 +106,46 @@ export class RequestComponent implements OnInit {
   }
 
   save() {
-    const email = this.loginForm.get("email").value;
-    const password = this.loginForm.get("password").value;
-    const rememberMe = this.loginForm.get("rememberMe").value;
+    console.log(this.requestForm.value);
+
+    let formattedDate = new Date(
+      this.requestForm.get("dob").value
+    ).toLocaleString();
+
+    var data = {
+      address1: this.requestForm.get("address1").value,
+      address2: this.requestForm.get("address2").value,
+      city: this.requestForm.get("city").value,
+      description: this.requestForm.get("description").value,
+      disbursed: this.requestForm.get("disbursed").value,
+      dob: formattedDate,
+      email: this.requestForm.get("email").value,
+      working_status: this.requestForm.get("working_status").value,
+      first_name: this.requestForm.get("first_name").value,
+      interest: this.requestForm.get("interest").value,
+      last_name: this.requestForm.get("last_name").value,
+      title: this.requestForm.get("title").value,
+      loan_product: this.requestForm.get("loan_product").value,
+      middle_name: this.requestForm.get("middle_name").value,
+      mobile: this.requestForm.get("mobile").value,
+      postal_code: this.requestForm.get("postal_code").value,
+      state: this.requestForm.get("state").value,
+      userId: this.userId,
+    };
+
+    console.log(data);
 
     this.loading = true;
-    // this.authenticationService.login(email.toLowerCase(), password).subscribe(
-    //   (data) => {
-    //     if (rememberMe) {
-    //       localStorage.setItem("savedUserEmail", email);
-    //     } else {
-    //       localStorage.removeItem("savedUserEmail");
-    //     }
-    //     this.router.navigate(["/"]);
-    //   },
-    //   (error) => {
-    //     this.notificationService.openSnackBar(error.error);
-    //     this.loading = false;
-    //   }
-    // );
+    this.loanService.borrowerSave(data).subscribe(
+      (data) => {
+        console.log(data);
+
+        this.notificationService.openSnackBar("request saved successfully");
+      },
+      (error) => {
+        this.loading = false;
+      }
+    );
   }
 
   resetPassword() {
